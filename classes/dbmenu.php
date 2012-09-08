c56ad50ab2ceac86ded83e4240d517ea3febb580
Marcus Reinhardt <webstone@gmail.com>
2012-09-08 19:48:40 +0300

    Update classes/dbmenu.php
    Added functionality to activate/deactivate bootstrap css via config
    and via function execution
    
    second parameter is optional and if it's empty the script will get the active status from the config file.

--- classes/dbmenu.php
+++ classes/dbmenu.php
@@ -15,18 +15,33 @@ class DbMenu {
     /**
 	* @var table name
 	*/
-	public static $table = null;
+	public static $table                = null;
+        public static $bootstrap            = null;
+        public static $ul_class             = null;
+        public static $first_class          = null;
+        public static $second_class         = null;
+        public static $dropdown_icon        = null;
+        public static $first_link_class     = null;
+        public static $first_link_toggle    = null;
 
     public static function _init()
-	{
-	   	\Config::load('dbmenu', true);
-		static::$table  = \Config::get('dbmenu.db.table', 'dbmenu');
-	}
-
-    public static function build($menu_name = 'main')
     {
+            \Config::load('dbmenu', true);
+            static::$table              = \Config::get('dbmenu.db.table', 'dbmenu');
+            static::$bootstrap          = \Config::get('dbmenu.bootstrap.active');
+            static::$ul_class           = \Config::get('dbmenu.bootstrap.ul_class');
+            static::$first_class        = \Config::get('dbmenu.bootstrap.first_class');
+            static::$second_class       = \Config::get('dbmenu.bootstrap.second_class');
+            static::$dropdown_icon      = \Config::get('dbmenu.bootstrap.dropdown_icon');
+            static::$first_link_class   = \Config::get('dbmenu.bootstrap.first_link_class');
+            static::$first_link_toggle  = \Config::get('dbmenu.bootstrap.first_link_toggle');
+    }
+
+    public static function build($menu_name = 'main', $bootstrap=null)
+    {
+        $bootstrap = (isset($bootstrap)) ? $bootstrap : static::$bootstrap;
         $menu_data = static::populate_menu($menu_name);
-        $html = static::build_menu($menu_data);
+        $html = static::build_menu($menu_data, 0, false, $bootstrap);
         return substr($html, 0, strlen($html)-5); // strip the last </ul> from the string
     }
 
@@ -56,7 +71,7 @@ class DbMenu {
      * Source:  http://twitter.github.com/bootstrap/components.html#dropdowns
      *          http://twitter.github.com/bootstrap/components.html#navbar 
      */
-    private static function build_menu($menu_data, $parent = 0, $sub = false)
+    private static function build_menu($menu_data, $parent = 0, $sub = false, $bootstrap)
     {
         $html = "";
         if (isset($menu_data['parents'][$parent]))
@@ -75,20 +90,20 @@ class DbMenu {
                     if($parent==0)
                     { 
                         //set the dropdown code for the 1st level
-                        $html .= '<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">'.$menu_data['items'][$itemId]['title'].'<b class="caret"></b></a>';
+                        $html .= '<li '.(($bootstrap) ? static::$first_class : "").'><a href="'.$menu_data['items'][$itemId]['link'].'" '.(($bootstrap) ? static::$first_link_class : "").' '.(($bootstrap) ? static::$first_link_toggle : "").'>'.$menu_data['items'][$itemId]['title'].''.(($bootstrap) ? static::$dropdown_icon : "").'</a>';
                     }
                     else
                     {
                         //set the dropdown code for the 2nd level
-                        $html .= '<li class="dropdown-submenu"><a href="'.$menu_data['items'][$itemId]['link'].'">'.$menu_data['items'][$itemId]['title'].'<b class="caret"></b></a>';
+                        $html .= '<li '.(($bootstrap) ? static::$second_class : "").'><a href="'.$menu_data['items'][$itemId]['link'].'">'.$menu_data['items'][$itemId]['title'].''.(($bootstrap) ? static::$dropdown_icon : "").'</a>';
                     }
                 
                     if ($parent === 0 || ($parent!=0 && $sub==true))
                     {
-                        $html .= '<ul class="dropdown-menu">';
+                        $html .= '<ul '.(($bootstrap) ? static::$ul_class : "").'>';
                     }
                     
-                    $html .= static::build_menu($menu_data, $itemId, true);
+                    $html .= static::build_menu($menu_data, $itemId, true, $bootstrap);
                     $html .= "</li>";
                 }
             }
@@ -101,4 +116,5 @@ class DbMenu {
         
         return $html;
     }
+
 }
